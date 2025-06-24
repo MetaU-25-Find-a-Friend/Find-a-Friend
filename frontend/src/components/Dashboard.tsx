f040ef53dc4586318d4e5f9a386ba25f331e8565
Leia Marie Spagnola <leia@meta.com>
2025-06-24 08:32:00 -0700

    add FA icons and move logout button to menu

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -2,12 +2,17 @@ import styles from "../css/Dashboard.module.css";
 import { useUser } from "../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 // Landing page; allows navigating to profile, map, etc.
 const Dashboard = () => {
     const { user } = useUser();
 
     const navigate = useNavigate();
+
+    const [showingMenu, setShowingMenu] = useState(false);
 
     const handleLogout = () => {
         logout();
@@ -30,12 +35,24 @@ const Dashboard = () => {
     } else {
         return (
             <main className={styles.grid}>
-                <h1 className={styles.title}>Find a Friend</h1>
-                <button
-                    className={styles.button}
-                    onClick={handleLogout}>
-                    Logout
-                </button>
+                <div className={styles.titleContainer}>
+                    <h1 className={styles.title}>Find a Friend</h1>
+                    <div className={styles.userMenuButton} onClick={() => {
+                        setShowingMenu(v => !v)
+                    }}>
+                        <FontAwesomeIcon
+                            icon={faUser}
+                            color="var(--teal-accent)"
+                            size="lg"></FontAwesomeIcon>
+                        <FontAwesomeIcon
+                            icon={faChevronDown}
+                            color="var(--teal-accent)"
+                            size="lg"></FontAwesomeIcon>
+                    </div>
+                    <div className={`${styles.userMenu} ${showingMenu ? styles.visible : styles.invisible}`}>
+                        <button className={styles.userMenuItem} onClick={handleLogout}>Logout</button>
+                    </div>
+                </div>
             </main>
         );
     }
