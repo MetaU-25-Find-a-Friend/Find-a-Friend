d54a4b0a1edd47bf0b649bd0bfdd00855464f1d2
Leia Marie Spagnola <leia@meta.com>
2025-07-01 16:54:11 -0700

    add getting list of friends and user data and basic UI to Messages page

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -2,17 +2,49 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useUser } from "../contexts/UserContext";
 import styles from "../css/Messages.module.css";
 import LoggedOut from "./LoggedOut";
-import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import {
+    faArrowLeftLong,
+    faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { AllUserData } from "../types";
+import { getAllData } from "../utils";
 
 /**
- * 
+ *
  * @returns A page where the user can view received messages and send messages to friends
  */
 const Messages = () => {
     const { user } = useUser();
 
     const navigate = useNavigate();
+
+    const [selectedFriendId, setSelectedFriendId] = useState<number | null>(
+        null,
+    );
+
+    // profiles of all of current user's friends
+    const [friends, setFriends] = useState(Array() as AllUserData[]);
+
+    const loadFriends = async () => {
+        if (user) {
+            const { friends } = await getAllData(user.id);
+
+            const result = Array() as AllUserData[];
+
+            for (const friend of friends) {
+                const data = await getAllData(friend);
+                result.push(data);
+            }
+
+            setFriends(result);
+        }
+    };
+
+    useEffect(() => {
+        loadFriends();
+    }, [user]);
 
     if (!user) {
         return <LoggedOut></LoggedOut>;
@@ -26,6 +58,36 @@ const Messages = () => {
                     Back to Dashboard
                 </button>
                 <h2 className={styles.title}>Messages</h2>
+                <div className={styles.friendsContainer}>
+                    {friends.map((friend) => (
+                        <div
+                            className={`${styles.friend} ${friend.id === selectedFriendId ? styles.selected : ""}`}
+                            onClick={() => setSelectedFriendId(friend.id)}>
+                            {friend.firstName} {friend.lastName}
+                        </div>
+                    ))}
+                </div>
+                {selectedFriendId ? (
+                    <div className={styles.rightBox}>
+                        <div className={styles.messages}></div>
+                        <div className={styles.inputContainer}>
+                            <input
+                                type="text"
+                                className={styles.input}
+                                placeholder="New message"></input>
+                            <button className={styles.sendButton}>
+                                <FontAwesomeIcon
+                                    icon={faArrowRight}
+                                    color="white"
+                                    size="2x"></FontAwesomeIcon>
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={styles.rightBox}>
+                        <p className={styles.noneSelected}>No user selected.</p>
+                    </div>
+                )}
             </div>
         );
     }
