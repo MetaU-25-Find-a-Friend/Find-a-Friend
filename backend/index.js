c805b3bc50990f9663c6153ecad9950728ad158d
Leia Marie Spagnola <leia@meta.com>
2025-07-08 11:39:18 -0700

    allow requests from all onrender subdomains (for PR previews)

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://find-a-friend-site.onrender.com",
+            /\.onrender\.com$/,
         ],
         credentials: true,
     }),
