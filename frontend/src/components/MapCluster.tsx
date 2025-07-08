53e979219a7dc5e2461cac7c5499c8585efaeb29
Leia Marie Spagnola <leia@meta.com>
2025-07-08 15:09:58 -0700

    show popups for all users in a cluster on hover

--- frontend/src/components/MapCluster.tsx
+++ frontend/src/components/MapCluster.tsx
@@ -1,7 +1,9 @@
 import { AdvancedMarker } from "@vis.gl/react-google-maps";
 import styles from "../css/MapCluster.module.css";
 import type { AllUserData, ClusterData } from "../types";
-import { geoHashToLatLng } from "../utils";
+import { geoHashToLatLng, getAllData } from "../utils";
+import { useEffect, useState } from "react";
+import ProfilePopup from "./ProfilePopup";
 
 interface MapClusterProps {
     cluster: ClusterData;
@@ -9,13 +11,27 @@ interface MapClusterProps {
 }
 
 const MapCluster = ({ cluster, setModalData }: MapClusterProps) => {
+    const [usersInCluster, setUsersInCluster] = useState(
+        Array() as AllUserData[],
+    );
+
+    useEffect(() => {
+        const result = Array() as AllUserData[];
+
+        for (const id of cluster.userIds) {
+            getAllData(id).then((data) => result.push(data));
+        }
+
+        setUsersInCluster(result);
+    }, []);
+
     return (
         <AdvancedMarker position={geoHashToLatLng(cluster.geohash)}>
             <div className={styles.cluster}>
                 <p className={styles.number}>{cluster.userIds.length}</p>
                 <div className={styles.popupList}>
-                    {cluster.userIds.map((id) => (
-                        <p>{id}</p>
+                    {usersInCluster.map((user) => (
+                        <ProfilePopup userData={user}></ProfilePopup>
                     ))}
                 </div>
             </div>
