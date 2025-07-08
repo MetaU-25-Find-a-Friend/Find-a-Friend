a47ba9e9a04280efaa3d69dd460dcce7d4fd0807
Leia Marie Spagnola <leia@meta.com>
2025-07-08 10:50:47 -0700

    create Loading component

--- frontend/src/components/RecommendationList.tsx
+++ frontend/src/components/RecommendationList.tsx
@@ -3,6 +3,7 @@ import { recommendPlaces, getNearbyPOIs } from "../recommendation-utils";
 import { useState } from "react";
 import type { PlaceRecData, UserGeohash } from "../types";
 import { useUser } from "../contexts/UserContext";
+import Loading from "./Loading";
 
 interface RecommendationListProps {
     myLocation: string;
@@ -24,55 +25,62 @@ const RecommendationList = ({
     // array of places combined with data on the number of users there and their similarity to the current user
     const [nearbyPlaces, setNearbyPlaces] = useState(Array() as PlaceRecData[]);
 
-    return (
-        <div
-            className={`${styles.placesListContainer} ${nearbyPlaces.length === 0 ? "" : styles.open}`}>
-            <div className={styles.placesList}>
-                <button
-                    className={styles.placesButton}
-                    onClick={() => {
-                        // get all places nearby
-                        getNearbyPOIs(myLocation)
-                            .then((places) =>
-                                // combine each place with data on users there
-                                recommendPlaces(
-                                    places,
-                                    user!.id,
-                                    myLocation,
-                                    otherUsers,
-                                ),
-                            )
-                            .then((placesWithUsers) =>
-                                // load data and display in list
-                                setNearbyPlaces(placesWithUsers),
-                            );
-                    }}>
-                    {nearbyPlaces.length === 0 ? "Load places" : "Reload"}
-                </button>
-                <p className={styles.explanation}>
-                    You're likely to find friends at these places nearby:
-                </p>
-                {nearbyPlaces.map((place) => (
-                    <div className={styles.place}>
-                        <h6 className={styles.placeName}>
-                            {place.place.displayName.text}
-                        </h6>
-                        <p className={styles.placeAddress}>
-                            {place.place.formattedAddress}
-                        </p>
-                        <p className={styles.userList}>
-                            {place.userData.count}{" "}
-                            {place.userData.count === 1
-                                ? "user is"
-                                : "users are"}{" "}
-                            here. You've been here {place.numVisits}{" "}
-                            {place.numVisits === 1 ? "time" : "times"} before.
-                        </p>
-                    </div>
-                ))}
+    const [loading, setLoading] = useState(true);
+
+    if (loading) {
+        return <Loading></Loading>;
+    } else {
+        return (
+            <div
+                className={`${styles.placesListContainer} ${nearbyPlaces.length === 0 ? "" : styles.open}`}>
+                <div className={styles.placesList}>
+                    <button
+                        className={styles.placesButton}
+                        onClick={() => {
+                            // get all places nearby
+                            getNearbyPOIs(myLocation)
+                                .then((places) =>
+                                    // combine each place with data on users there
+                                    recommendPlaces(
+                                        places,
+                                        user!.id,
+                                        myLocation,
+                                        otherUsers,
+                                    ),
+                                )
+                                .then((placesWithUsers) =>
+                                    // load data and display in list
+                                    setNearbyPlaces(placesWithUsers),
+                                );
+                        }}>
+                        {nearbyPlaces.length === 0 ? "Load places" : "Reload"}
+                    </button>
+                    <p className={styles.explanation}>
+                        You're likely to find friends at these places nearby:
+                    </p>
+                    {nearbyPlaces.map((place) => (
+                        <div className={styles.place}>
+                            <h6 className={styles.placeName}>
+                                {place.place.displayName.text}
+                            </h6>
+                            <p className={styles.placeAddress}>
+                                {place.place.formattedAddress}
+                            </p>
+                            <p className={styles.userList}>
+                                {place.userData.count}{" "}
+                                {place.userData.count === 1
+                                    ? "user is"
+                                    : "users are"}{" "}
+                                here. You've been here {place.numVisits}{" "}
+                                {place.numVisits === 1 ? "time" : "times"}{" "}
+                                before.
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default RecommendationList;
