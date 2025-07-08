02ab445d9f67d7637b5a4f9d5f1ccbdd524c9244
Leia Marie Spagnola <leia@meta.com>
2025-07-08 10:59:04 -0700

    add animated bar

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,7 +1,14 @@
 import styles from "../css/Loading.module.css";
 
 const Loading = () => {
-    return <div className={styles.container}>Loading</div>;
+    return (
+        <div className={styles.container}>
+            <h3 className={styles.text}>Loading...</h3>
+            <div className={styles.bar}>
+                <div className={styles.slider}></div>
+            </div>
+        </div>
+    );
 };
 
 export default Loading;
