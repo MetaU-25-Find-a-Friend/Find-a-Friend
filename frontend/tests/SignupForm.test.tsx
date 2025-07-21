231e385f9977703f33bcd2a8af1163ca052fcbea
Leia Marie Spagnola <leia@meta.com>
2025-07-21 15:31:30 -0700

    add signup form tests

---
+++ frontend/tests/SignupForm.test.tsx
@@ -0,0 +1,105 @@
+import { afterAll, afterEach, describe, expect, it, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { createAccount } from "../src/utils";
+import SignupForm from "../src/components/SignupForm";
+
+// mock createAccount to prevent backend fetch and get call data
+vi.mock("../src/utils", async (importOriginal) => {
+    return {
+        ...(await importOriginal<typeof import("../src/utils")>()),
+        createAccount: vi.fn(
+            (accountData: {
+                firstName: string;
+                lastName: string;
+                email: string;
+                password: string;
+            }) => [true, "ok"],
+        ),
+    };
+});
+
+// mock useNavigate since its real implementation can only be called from inside a Router
+vi.mock("react-router-dom", async (importOriginal) => {
+    return {
+        ...(await importOriginal<typeof import("react-router-dom")>()),
+        useNavigate: vi.fn(() => () => {}),
+    };
+});
+
+describe("Create account page", () => {
+    afterEach(() => vi.clearAllMocks());
+
+    afterAll(() => vi.restoreAllMocks());
+
+    it("renders the form", () => {
+        render(<SignupForm></SignupForm>);
+
+        // the below calls will throw errors if any of these elements are not present
+        screen.getByPlaceholderText(/^First name$/);
+        screen.getByPlaceholderText(/^Last name$/);
+        screen.getByPlaceholderText(/^Email$/);
+        screen.getByPlaceholderText(/^Password$/);
+        screen.getByPlaceholderText(/^Confirm password$/);
+
+        screen.getByText(/^Create Account$/);
+
+        screen.getByText(/^Login to existing account$/);
+    });
+
+    it("tries to submit on button click", () => {
+        render(<SignupForm></SignupForm>);
+
+        // click on "Create Account" button
+        const submitButton = screen.getByText(/^Create Account$/);
+        fireEvent.click(submitButton);
+
+        // should call createAccount
+        expect(createAccount).toHaveBeenCalledOnce();
+    });
+
+    it("submits with user input", () => {
+        render(<SignupForm></SignupForm>);
+
+        // change text in name and email textboxes
+        const firstName = screen.getByPlaceholderText(/^First name$/);
+        fireEvent.change(firstName, { target: { value: "Test" } });
+
+        const lastName = screen.getByPlaceholderText(/^Last name$/);
+        fireEvent.change(lastName, { target: { value: "User" } });
+
+        const email = screen.getByPlaceholderText(/^Email$/);
+        fireEvent.change(email, { target: { value: "test@test.com" } });
+
+        // click on "Create Account" button
+        const submitButton = screen.getByText(/^Create Account$/);
+        fireEvent.click(submitButton);
+
+        // createAccount should be called with text entered in form
+        expect(createAccount).toHaveBeenCalledExactlyOnceWith({
+            firstName: "Test",
+            lastName: "User",
+            email: "test@test.com",
+            password: "",
+            confirmPassword: "",
+        });
+    });
+
+    it("doesn't submit if passwords are different", () => {
+        render(<SignupForm></SignupForm>);
+
+        // set password text to "1" and confirm password text to "2"
+        const password = screen.getByPlaceholderText(/^Password$/);
+        fireEvent.change(password, { target: { value: "1" } });
+
+        const confirmPassword =
+            screen.getByPlaceholderText(/^Confirm password$/);
+        fireEvent.change(confirmPassword, { target: { value: "2" } });
+
+        // click on "Create Account" button
+        const submitButton = screen.getByText(/^Create Account$/);
+        fireEvent.click(submitButton);
+
+        // click handler should notice error and not call createAccount
+        expect(createAccount).toHaveBeenCalledTimes(0);
+    });
+});
