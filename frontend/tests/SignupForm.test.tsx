ff3654ce5dd9fd7d39e08c3b9aec1fdaff325075
Leia Marie Spagnola <leia@meta.com>
2025-07-22 10:48:22 -0700

    add and improve signup form tests

--- frontend/tests/SignupForm.test.tsx
+++ frontend/tests/SignupForm.test.tsx
@@ -1,5 +1,5 @@
 import { afterAll, afterEach, describe, expect, it, vi } from "vitest";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { createAccount } from "../src/utils";
 import SignupForm from "../src/components/SignupForm";
 
@@ -18,11 +18,13 @@ vi.mock("../src/utils", async (importOriginal) => {
     };
 });
 
+const mockNavigate = vi.fn((path: string) => {});
+
 // mock useNavigate since its real implementation can only be called from inside a Router
 vi.mock("react-router-dom", async (importOriginal) => {
     return {
         ...(await importOriginal<typeof import("react-router-dom")>()),
-        useNavigate: vi.fn(() => () => {}),
+        useNavigate: () => mockNavigate,
     };
 });
 
@@ -60,27 +62,36 @@ describe("Create account page", () => {
     it("submits with user input", () => {
         render(<SignupForm></SignupForm>);
 
+        const testInputs = {
+            firstName: "Test",
+            lastName: "User",
+            email: "test@test.com",
+            password: "",
+            confirmPassword: "",
+        };
+
         // change text in name and email textboxes
         const firstName = screen.getByPlaceholderText(/^First name$/);
-        fireEvent.change(firstName, { target: { value: "Test" } });
+        fireEvent.change(firstName, {
+            target: { value: testInputs.firstName },
+        });
 
         const lastName = screen.getByPlaceholderText(/^Last name$/);
-        fireEvent.change(lastName, { target: { value: "User" } });
+        fireEvent.change(lastName, { target: { value: testInputs.lastName } });
 
         const email = screen.getByPlaceholderText(/^Email$/);
-        fireEvent.change(email, { target: { value: "test@test.com" } });
+        fireEvent.change(email, { target: { value: testInputs.email } });
 
         // click on "Create Account" button
         const submitButton = screen.getByText(/^Create Account$/);
         fireEvent.click(submitButton);
 
         // createAccount should be called with text entered in form
-        expect(createAccount).toHaveBeenCalledExactlyOnceWith({
-            firstName: "Test",
-            lastName: "User",
-            email: "test@test.com",
-            password: "",
-            confirmPassword: "",
+        expect(createAccount).toHaveBeenCalledExactlyOnceWith(testInputs);
+
+        // navigate should be called after createAccount completes
+        waitFor(() => {
+            expect(mockNavigate).toHaveBeenCalledExactlyOnceWith("/login");
         });
     });
 
@@ -101,5 +112,38 @@ describe("Create account page", () => {
 
         // click handler should notice error and not call createAccount
         expect(createAccount).toHaveBeenCalledTimes(0);
+    });
+
+    it("navigates to login", () => {
+        render(<SignupForm></SignupForm>);
+
+        // click on "Login to existing account" button
+        const login = screen.getByText(/^Login to existing account$/);
+        fireEvent.click(login);
+
+        // navigate should be called
+        expect(mockNavigate).toHaveBeenCalledExactlyOnceWith("/login");
+    });
+
+    it("shows error alert", () => {
+        render(<SignupForm></SignupForm>);
+
+        // click on "Create account" button
+        const submitButton = screen.getByText(/^Create Account$/);
+        fireEvent.click(submitButton);
+
+        const testErrorMessage = "Error";
+
+        // mock a failed signup
+        // @ts-ignore since TS doesn't recognize createAccount as a mock
+        createAccount.mockImplementationOnce(() => [false, testErrorMessage]);
+
+        // createAccount should be called
+        expect(createAccount).toHaveBeenCalledOnce();
+
+        // alert should show once createAccount completes
+        waitFor(() => {
+            screen.getByText(testErrorMessage);
+        });
     });
 });
