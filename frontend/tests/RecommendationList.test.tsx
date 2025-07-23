d1949b1829770fac6eb4d5384d0581cd001e49ce
Leia Marie Spagnola <leia@meta.com>
2025-07-23 14:57:11 -0700

    add feedback tests

--- frontend/tests/RecommendationList.test.tsx
+++ frontend/tests/RecommendationList.test.tsx
@@ -1,13 +1,16 @@
-import { vi, describe, it, afterAll, afterEach } from "vitest";
+import { vi, describe, it, afterAll, afterEach, expect } from "vitest";
 import {
     Place,
     PlaceRecData,
     PlaceRecStats,
     SavedUser,
     UserGeohash,
+    WeightAdjustments,
 } from "../src/types";
 import RecommendationList from "../src/components/RecommendationList";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { addLikedType, updateWeights } from "../src/recommendation-utils";
+import { LIKED_WEIGHT_INCREASE } from "../src/constants";
 
 // mock recommendation functions to prevent fetches and calculation
 vi.mock("../src/recommendation-utils", async (importOriginal) => {
@@ -56,7 +59,7 @@ vi.mock("../src/recommendation-utils", async (importOriginal) => {
                             visitScore: 0,
                             isLikedType: false,
                             userData: {
-                                count: 0,
+                                count: 2,
                                 avgSimilarity: 0,
                                 friendCount: 0,
                             },
@@ -64,6 +67,13 @@ vi.mock("../src/recommendation-utils", async (importOriginal) => {
                         },
                     ],
                 ]),
+        ),
+        updateWeights: vi.fn(
+            async (adjustments: WeightAdjustments) =>
+                await Promise.resolve(true),
+        ),
+        addLikedType: vi.fn(
+            async (type: string) => await Promise.resolve(true),
         ),
     };
 });
@@ -133,5 +143,65 @@ describe("Recommendation list", () => {
             screen.getByText(/^More popular$/);
             screen.getByText(/^In my history$/);
         });
+    });
+
+    it("tries to update weights when feedback button is clicked", async () => {
+        render(
+            <RecommendationList
+                myLocation="9h9j5bbbb"
+                otherUsers={[]}></RecommendationList>,
+        );
+
+        // click "Load places" button
+        const load = screen.getByText(/^Load places$/);
+        fireEvent.click(load);
+
+        // click "Closer" button
+        const closer = await waitFor(() => {
+            const element = screen.getByText(/^Closer$/);
+            return element;
+        });
+
+        fireEvent.click(closer);
+
+        // updateWeights should be called with an increase in the distance weight
+        expect(updateWeights).toHaveBeenCalledExactlyOnceWith({
+            distanceAdjustment: 1,
+        });
+    });
+
+    it("tries to update weights when like button is clicked", async () => {
+        render(
+            <RecommendationList
+                myLocation="9h9j5bbbb"
+                otherUsers={[]}></RecommendationList>,
+        );
+
+        // click "Load places" button
+        const load = screen.getByText(/^Load places$/);
+        fireEvent.click(load);
+
+        // click like button (sibling of the place name)
+        const like = await waitFor(() => {
+            const element =
+                screen.getByText(/^Test Place$/).nextElementSibling!;
+            return element;
+        });
+
+        fireEvent.click(like);
+
+        // updateWeights should be called with an increase in the count weight,
+        // since the mocked place has an above-average user count
+        expect(updateWeights).toHaveBeenCalledExactlyOnceWith({
+            friendAdjustment: 0,
+            pastVisitAdjustment: 0,
+            countAdjustment: LIKED_WEIGHT_INCREASE,
+            similarityAdjustment: 0,
+            distanceAdjustment: 0,
+            typeAdjustment: 0,
+        });
+
+        // addLikedType should be called to attempt to add "type"
+        expect(addLikedType).toHaveBeenCalledExactlyOnceWith("type");
     });
 });
