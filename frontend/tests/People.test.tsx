826fd7c73c8e9a79968b6ddc8142e8384d5f3964
Leia Marie Spagnola <leia@meta.com>
2025-07-25 15:39:54 -0700

    test sending friend request

--- frontend/tests/People.test.tsx
+++ frontend/tests/People.test.tsx
@@ -7,13 +7,13 @@ import {
     SavedUser,
     SuggestedProfile,
 } from "../src/types";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import {
     addConnectionsToCache,
     getSuggestedPeople,
     removeConnectionsFromCache,
 } from "../src/people-utils";
-import { getAllData } from "../src/utils";
+import { getAllData, sendFriendRequest } from "../src/utils";
 
 const mockNavigate = vi.fn((path: string) => {});
 
@@ -56,6 +56,10 @@ vi.mock("../src/utils", async (importOriginal) => {
                     ...mockUserData,
                 }),
         ),
+        sendFriendRequest: vi.fn(
+            async (id: number) => await Promise.resolve(true),
+        ),
+        blockUser: vi.fn(async (id: number) => await Promise.resolve(true)),
     };
 });
 
@@ -79,7 +83,6 @@ vi.mock("../src/people-utils", async (importOriginal) => {
                             },
                         ],
                     },
-
                     {
                         data: {
                             id: id + 1,
@@ -347,5 +350,22 @@ describe("People page", () => {
         expect(getSuggestedPeople).toHaveBeenCalledOnce();
         // should have tried to remove the blocked user's connections
         expect(removeConnectionsFromCache).toHaveBeenCalledOnce();
+    });
+
+    it("tries to send a friend request", async () => {
+        render(
+            <PeopleProvider>
+                <People></People>
+            </PeopleProvider>,
+        );
+
+        // click the first "Send friend request" button
+        const requestButtons = await waitFor(() => {
+            return screen.getAllByText(/^Send friend request$/);
+        });
+        fireEvent.click(requestButtons[0]);
+
+        // should call sendFriendRequest
+        expect(sendFriendRequest).toHaveBeenCalledOnce();
     });
 });
