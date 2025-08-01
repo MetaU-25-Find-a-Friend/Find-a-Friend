4a62547f9066d98517a82c6ef60097e9b8020518
Leia Marie Spagnola <leia@meta.com>
2025-08-01 09:28:49 -0700

    fix mocks in modal tests

--- frontend/tests/Modal.test.tsx
+++ frontend/tests/Modal.test.tsx
@@ -45,18 +45,13 @@ vi.mock("../src/utils", async (importOriginal) => {
     return {
         ...(await importOriginal<typeof import("../src/utils")>()),
         getAllData: vi.fn(
-            async (id: number): Promise<AllUserData> =>
-                await Promise.resolve(mockCurrentUserData),
+            async (): Promise<AllUserData> =>
+                Promise.resolve(mockCurrentUserData),
         ),
-        sendFriendRequest: vi.fn(
-            async (id: number) => await Promise.resolve(true),
-        ),
-        blockUser: vi.fn(async (id: number) => await Promise.resolve(true)),
-        unblockUser: vi.fn(async (id: number) => await Promise.resolve(true)),
-        sendMessage: vi.fn(
-            async (to: number, text: string) =>
-                await Promise.resolve([true, "ok"]),
-        ),
+        sendFriendRequest: vi.fn(async () => Promise.resolve(true)),
+        blockUser: vi.fn(async () => Promise.resolve(true)),
+        unblockUser: vi.fn(async () => Promise.resolve(true)),
+        sendMessage: vi.fn(async () => Promise.resolve([true, "ok"])),
     };
 });
 
